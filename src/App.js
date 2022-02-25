b38f23a20953f8d83a8364c9bfef0e976f71506b
Ali Sariyer <alisariyer@gmail.com>
2022-02-25 21:58:44 +0000

    1. Add nanoid package 2. Change each die to an object and add also id with nanoid function 3. Add handleHeld function to handle click on die, getting die id and change object 4. Add a conditional styling in Die component if die's isHeld value is true change bgcolor

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React, { useState } from "react";
 import Die from "./Die";
+import { nanoid } from "nanoid";
 
 export default function App() {
+
   function allNewDice() {
     // TIP: [...Array(10).keys()] to create a list [0, 1, ... 10]
-    // create an array contains 10 random number 1 to 6 inclusive
-    return Array.from('helloworld').map(x => Math.ceil(Math.random() * 6));
+    // create an array of object to keep 10 random numbers and held status
+    return Array.from('helloworld').map(x => ({ 
+        value: Math.ceil(Math.random() * 6), 
+        isHeld: false,
+        id: nanoid()
+    }));
   }
 
   const [dice, setDice] = useState(() => allNewDice());
 
-  const diceElements = dice.map((die, index) => <Die key={index} value={die} />);
+  const diceElements = dice.map((die, index) => <Die key={index} die={die} handleHeld={handleHeld}/>);
 
   function rollDice() {
       setDice(allNewDice());
+  }
+
+  function handleHeld(id) {
+      setDice(prevDice => {
+          return prevDice.map(dice => {
+              if (dice.id === id) {
+                  return {
+                      ...dice,
+                      isHeld: !dice.isHeld
+                  }
+              }
+              return dice;
+          })
+      })
   }
 
   return (
@@ -21,7 +41,7 @@ export default function App() {
       <div className="die-container">
         {diceElements}
       </div>
-      <button className="btn-roll" type="button" onClick={rollDice}>Roll Dice</button>
+      <button className="btn-roll" type="button" onClick={rollDice}>Roll</button>
     </main>
   );
 }
--- src/Die.js
+++ src/Die.js
@@ -1,7 +1,11 @@
 import React from "react"
 
-export default function Die({ value }) {
+export default function Die({ die, handleHeld }) {
+    const style = {
+        backgroundColor: die.isHeld ? "#59E391" : "#FFF"
+    }
+    console.log(die);
     return (
-        <div className="die-box">{value}</div>
+        <div className="die-box" style={style} onClick={() => handleHeld(die.id)}>{die.value}</div>
     )
 }
