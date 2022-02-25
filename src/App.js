c590a63aa56c1a0d869a771e6c76493d26edabfd
Ali Sariyer <alisariyer@gmail.com>
2022-02-25 21:11:23 +0000

    Increase die-box size, create allNewDice function to get 10 random number list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from "react";
 import Die from "./Die";
 
 export default function App() {
+  function allNewDice() {
+    // TIP: [...Array(10).keys()] to create a list [0, 1, ... 10]
+    // create an array contains 10 random number 1 to 6 inclusive
+    return Array.from('helloworld').map(x => Math.ceil(Math.random() * 6));
+  }
+
+  console.log(allNewDice())
+
   return (
     <main>
       <div className="die-container">
